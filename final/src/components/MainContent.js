c4e881ac4de7f7958a15c255e860829a1d618f7a
mincheol11 <f438146@gmail.com>
2024-10-17 12:10:12 +0900

    언어 변경 기능 구현중

--- final/src/components/MainContent.js
+++ final/src/components/MainContent.js
@@ -8,8 +8,10 @@ import PaymentConfirmationPage from "./payment/PaymentConfirmationPage";
 import PaymentSuccessPage from "./payment/PaymentSuccessPage";
 import PrivateRoute from "../router/PrivateRoute";
 import PaymentCancellationPage from "./payment/PaymentCancellationPage";
+import { useTranslation } from 'react-i18next';
 
 const MainContent = () => {
+    const { t } = useTranslation(); // 번역 훅 사용
     //recoil에서 login 상태를 불러온다
     const login = useRecoilValue(loginState);
 
@@ -26,39 +28,18 @@ const MainContent = () => {
 
                     {/* 로그인이 필요한 페이지라면 element에 PrivateRoute를 적어서 대상을 명시하면 된다 */}
 
-
-
-                    {/* 경로변수를 사용할 경우 콜론과 이름을 합쳐 변수명으로 지정 */}
-
-                        {/* 결제 */}
-                        <Route path="/payment/method" element={<PaymentMethodPage/>}/>
-                        <Route path="/payment/confirmation" element={<PaymentConfirmationPage/>}/>
-                        <Route path="/payment/confirmation/success/:partnerOrderId" element={<PaymentSuccessPage />} />
-                        <Route path="/payment/cancel" element={<PaymentCancellationPage />} />
-
-
-                    {/* 기존 : 일반 라우팅 */}
-                    {/* <Route path="/search/autocomplete" element={<AutoComplete/>}/> */}
-                    {/* <Route path="/search/autocomplete2" element={<AutoComplete2/>}/> */}
-                    {/* <Route path="/search/member" element={<MemberComplexSearch/>}/> */}
-
-                    {/* 변경 : 중첩 라우팅 */}
-
-                    {/* 회원 로그인 */}
-                    <Route path="/member/login" element={<MemberLogin />} />
-
-
                     {/* 결제 */}
                     <Route path="/payment/method" element={<PaymentMethodPage />} />
                     <Route path="/payment/confirmation" element={<PaymentConfirmationPage />} />
                     <Route path="/payment/confirmation/success/:partnerOrderId" element={<PaymentSuccessPage />} />
+                    <Route path="/payment/cancel" element={<PaymentCancellationPage />} />
 
-                    {/* 나머지 경로(*) 패턴을 지정해서 미 지정된 페이지를 모두 연결할 수 있다 */}
+                    {/* 회원 로그인 */}
+                    <Route path="/member/login" element={<MemberLogin />} />
                 </Routes>
 
             </div>
         </div>
     </>);
 };
-
 export default MainContent;
