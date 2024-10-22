35978caa6ad0fac3be275495fcd4303a7216c471
mincheol11 <f438146@gmail.com>
2024-10-22 13:27:57 +0900

    footer에 언어 번역 기능 추가

--- final/src/components/footer/Footer.js
+++ final/src/components/footer/Footer.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import { NavLink } from "react-router-dom";
 import styles from './Footer.module.css';
+import { useTranslation } from 'react-i18next';
 
 const Footer = () => {
+  const { t } = useTranslation();
+
   return (
     <footer className={styles.footerContainer}>
       <div className={styles.footerContent}>
-        <div className={styles.footerLogo}>
-          <img src="/images/valve_logo.png" alt="Valve Logo" />
-        </div>
         <div className={styles.footerCopyright}>
-          <p>© 2024 Valve Corporation. All rights reserved. 모든 상표는 미국 및 기타 국가에서 해당하는 소유자의 재산입니다.<br/>
-          부가가치세 포함 | <NavLink to="/privacy-policy" className={styles.footerLink}>개인정보 처리방침</NavLink> | <NavLink to="/terms-of-use" className={styles.footerLink}>사용권</NavLink> | <NavLink to="/steam-agreement" className={styles.footerLink}>Steam 이용 약관</NavLink> | <NavLink to="/refund-policy" className={styles.footerLink}>환불</NavLink></p>
+          <p>© 2024 Valve Corporation. {t('footer.allRightsReserved')}<br/>
+          {t('footer.vatIncluded')} | <NavLink to="/privacy-policy" className={styles.footerLink}>{t('footer.privacyPolicy')}</NavLink> | <NavLink to="/terms-of-use" className={styles.footerLink}>{t('footer.termsOfUse')}</NavLink> | <NavLink to="/steam-agreement" className={styles.footerLink}>{t('footer.steamAgreement')}</NavLink> | <NavLink to="/refund-policy" className={styles.footerLink}>{t('footer.refundPolicy')}</NavLink></p>
         </div>
-       
       </div>
     </footer>
   );
