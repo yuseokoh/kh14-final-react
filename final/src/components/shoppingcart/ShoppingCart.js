7dfeaff54831573297d33ab1ed95f55bf242f433
poiu1061 <ryoupr1061@gmail.com>
2024-10-23 10:04:23 +0900

    장바구니

--- final/src/components/shoppingcart/ShoppingCart.js
+++ final/src/components/shoppingcart/ShoppingCart.js
@@ -6,7 +6,7 @@ import { loginState, memberIdState, memberLoadingState } from "../../utils/recoi
 
 const ShoppingCart = () => {
   const [cartList, setCartList] = useState([]);
-  
+
   // Recoil 상태 사용
   const login = useRecoilValue(loginState);
   const memberId = useRecoilValue(memberIdState);
@@ -14,31 +14,9 @@ const ShoppingCart = () => {
 
   // 장바구니 리스트 불러오는 함수
   const loadCartList = useCallback(async () => {
-    // try {
-    //   let token = localStorage.getItem("accessToken") || sessionStorage.getItem("accessToken");
-  
-    //   if (!token) {
-    //     const refreshToken = localStorage.getItem("refreshToken") || sessionStorage.getItem("refreshToken");
-  
-    //     if (refreshToken) {
-    //       const resp = await axios.post('/member/refresh', { refreshToken });
-    //       token = resp.data.accessToken;
-    //       localStorage.setItem('accessToken', token);
-    //     } else {
-    //       console.error("No access token found");
-    //       return;
-    //     }
-    //   }
-  
-      const resp = await axios.get("/cart/")
-      console.log(resp.data);
-      // , {
-      //   headers: { Authorization: `Bearer ${token}` }
-      // });
-      setCartList(resp.data);
-    // } catch (error) {
-    //   console.error("Failed to load cart data", error);
-    // }
+    const resp = await axios.get("/cart/");
+    console.log(resp.data);
+    setCartList(resp.data);
   }, []);
 
   useEffect(() => {
@@ -48,25 +26,27 @@ const ShoppingCart = () => {
   }, [login, memberId, loadCartList]);
 
   return (
-    <div className="row mt-4">
-      <div className="col">
-        <table className="table text-nowrap">
-          <thead>
-            <tr>
-              <th>게임이름</th>
-              <th>가격</th>
-            </tr>
-          </thead>
-          <tbody>
-            {cartList.map(cart => (
-              <tr key={cart.memberId}>
-                <td>{cart.gameTitle} </td>
-                <td>{cart.gamePrice.toLocaleString()} $</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+    <div className={styles.cartContainer}>
+      {cartList.map(cart => (
+        <div key={cart.cartId} className={styles.cartItem}>
+          {/* 게임 썸네일 */}
+          <img src={cart.gameThumbnail} alt={cart.gameTitle} className={styles.gameThumbnail} />
+
+          {/* 게임 정보 및 가격 */}
+          <div className={styles.gameInfo}>
+            <h4 className={styles.gameTitle}>{cart.gameTitle}</h4>
+            <p className={styles.gamePrice}>{cart.gamePrice.toLocaleString()}$</p>
+          </div>
+
+          {/* 추가 기능 (선물용 버튼 등) */}
+          <div className={styles.actionButtons}>
+            <button className={styles.giftButton}>선물용</button>
+            <button className={styles.removeButton}>제거</button>
+          </div>
+        </div>
+      ))}
+
+      <button type='button'>결제하기</button>
     </div>
   );
 };
